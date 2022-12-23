ec8e7c5ab9765f562f36725a955809ffa740a016
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-12-23 11:49:23 +0200

    create

---
+++ doc_gen/js/copy.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+  var src = document.getElementById("main_row"),
+      date = document.getElementById("date");
+        en = document.getElementById("en");
+       ru = document.getElementById("ru");
+       rec = document.getElementById("record_row");
+  src.addEventListener('input', function() {
+	  date.value = src.value;
+        en.value = src.value;
+        ru.value = src.value;
+        rec.value = src.value;
+    });
+  
+  main_row.addEventListener('input', function() {
+    en.value = translitRuEn(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    ru.value = translitRuRu(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    date.value = translitRu0(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    rec.value = translitRu0(this.value);
+  });
+
+  
+  
+};
+
+document.getElementById('main_row').addEventListener('input', function(e){
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+    e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split(' ').join('\n');
+});
+
+    
+main_row.addEventListener('input', function() {
+    record_row.value = translit2(this.value);
+  });
---
+++ quotes/script.js
@@ -0,0 +1,25 @@
+var randomQuote = "";
+
+
+function getQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+      method: "GET",
+      dataType: "jsonp",
+      success: function(request) {
+        randomQuote = request.quoteText;
+        
+        $('#header').html(randomQuote);
+        
+ },
+      error: function(xhr, status, error) 
+{
+    $('#quoteText').text('Not sure what happened there! Click again to generate a new quote!');
+       
+}
+  });
+}
+  
+
+
+      getQuote();
