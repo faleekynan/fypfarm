27b49c9c0c1d12a8365cdf4cd757cb595ebe78a3
faleekynan <faleekynan@gmail.com>
2022-12-26 17:41:21 +0200

    Новые поля

--- doc/js/copy.js
+++ doc/js/copy.js
@@ -4,12 +4,40 @@ window.onload = function() {
         en = document.getElementById("en");
        ru = document.getElementById("ru");
        rec = document.getElementById("record_row");
+	   
   src.addEventListener('input', function() {
 	  date.value = src.value;
         en.value = src.value;
         ru.value = src.value;
         rec.value = src.value;
+		
     });
+	
+	var name = document.getElementById("name");
+	    surname = document.getElementById("surname");
+		bir_date = document.getElementById("bir_date");
+	    paste_name = document.getElementById("paste_name");
+		paste_surname = document.getElementById("paste_surname");
+		paste_date = document.getElementById("paste_date");
+		
+	    en_paste_name = document.getElementById("en_paste_name");
+		en_paste_surname = document.getElementById("en_paste_surname");
+		reverse_bir_date = document.getElementById("reverse_bir_date");
+		
+      
+  name.addEventListener('input', function() {
+	paste_name.value = name.value;
+	en_paste_name.value = name.value;
+    });
+  surname.addEventListener('input', function() {
+	paste_surname.value = surname.value;
+	en_paste_surname.value = surname.value;
+	});
+  bir_date.addEventListener('input', function() {
+	paste_date.value = bir_date.value;
+	reverse_bir_date.value = bir_date.value;
+    });
+	
   
   main_row.addEventListener('input', function() {
     en.value = translitRuEn(this.value);
@@ -23,7 +51,17 @@ window.onload = function() {
   main_row.addEventListener('input', function() {
     rec.value = translitRu0(this.value);
   });
-
+  
+  name.addEventListener('input', function() {
+    en_paste_name.value = translitRuEn(this.value);
+  });
+  surname.addEventListener('input', function() {
+    en_paste_surname.value = translitRuEn(this.value);
+  });
+bir_date.addEventListener('input', function() {
+    paste_date.value = translitRu0(this.value);
+	reverse_bir_date.value = translitRu0(this.value);
+  });
   
   
 };
@@ -42,3 +80,5 @@ document.getElementById('main_row').addEventListener('input', function(e){
 main_row.addEventListener('input', function() {
     record_row.value = translit2(this.value);
   });
+ 
+
