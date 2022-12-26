9d017ca86d29f23225e7192d9d9e0f845ad294f3
faleekynan <faleekynan@gmail.com>
2022-12-26 19:44:06 +0200

    отчество

--- doc/js/checkbox.js
+++ doc/js/checkbox.js
@@ -4,19 +4,23 @@ $("input[type='radio']").change(function(){
 if($(this).val()=="f")
 {
 $("#f").show();
+$("#middle_name").show();
 }
 else
 {
-$("#f").hide();       
+$("#f").hide(); 
+$("#middle_name").hide();       
 }
 
 if($(this).val()=="m")
 {   
-$("#m").show(); 
+$("#m").show();
+$("#middle_name_m").show();  
 }
 else
 {
 $("#m").hide();
+$("#middle_name_m").hide();
 }
 
   
